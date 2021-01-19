b135e1cb1a143b40038ee0a121ed641811cc86f2
Guillaume <guillaume.billey@gmail.com>
2021-01-19 11:44:34 +0100

    fix(board): use directionnal graph, and simplify graph creation

--- src/board.py
+++ src/board.py
@@ -7,21 +7,19 @@ class Board:
     def __init__(self, width=7, height=7):
         self.WIDTH = width
         self.HEIGHT = height
-        self.graph = nx.Graph()
+        self.graph = nx.DiGraph()
 
         for x in range(self.WIDTH):
             for y in range(self.HEIGHT):
-                self.graph.add_node((x, y))
+                if (x > 0):
+                    self.graph.add_edge((x, y), (x-1, y), direction=Board.WEST)
+                if (x < self.WIDTH - 1):
+                    self.graph.add_edge((x, y), (x+1, y), direction=Board.EAST)
+                if (y > 0):
+                    self.graph.add_edge((x, y), (x, y-1), direction=Board.NORTH)
+                if (y < self.HEIGHT - 1):
+                    self.graph.add_edge((x, y), (x, y+1), direction=Board.SOUTH)
 
-        for (x, y) in self.graph.nodes:
-            if (x > 0):
-                self.graph.add_edge((x, y), (x-1, y), direction=Board.WEST)
-            if (x < self.WIDTH - 1):
-                self.graph.add_edge((x, y), (x+1, y), direction=Board.EAST)
-            if (y > 0):
-                self.graph.add_edge((x, y), (x, y-1), direction=Board.SOUTH)
-            if (y < self.HEIGHT - 1):
-                self.graph.add_edge((x, y), (x, y+1), direction=Board.NORTH)
 
     def display(self):
         print('# '*(self.WIDTH+2))
