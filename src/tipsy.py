041bd3d3e84ae911120fa38a8aaa7f637d4bce92
Guillaume <guillaume.billey@gmail.com>
2021-01-19 14:20:30 +0100

    feat(input): while loop with input awaiting

--- src/tipsy.py
+++ src/tipsy.py
@@ -2,4 +2,9 @@ from board import Board
 
 board = Board()
 
-board.display()
+while True:
+    board.display()
+    input_command = ''
+    while (input_command.lower() not in ['e', 'w', 'n', 's']):
+        input_command = raw_input("Use N,S,E,W keys to tilt the board: ")
+    print('move to ' + input_command)
