f49c6ac41c4af1541488d33c17209cec5cac4d89
Guillaume <guillaume.billey@gmail.com>
2021-01-19 15:35:32 +0100

    fix(input): replace raw_input with input
    python 3 does not  support raw_input anymore

--- src/tipsy.py
+++ src/tipsy.py
@@ -7,7 +7,7 @@ while True:
     input_command = ''
 
     while (input_command.lower() not in [Board.EAST, Board.WEST, Board.NORTH, Board.SOUTH]):
-        input_command = raw_input('Use ' + Board.NORTH + ', ' + Board.SOUTH + ', ' + Board.EAST + ', ' + Board.WEST + ' to tilt the board: ')
+        input_command = input('Use ' + Board.NORTH + ', ' + Board.SOUTH + ', ' + Board.EAST + ', ' + Board.WEST + ' to tilt the board: ')
     print('move to ' + input_command)
 
     board.tilt(input_command);
